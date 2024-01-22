a248e28b59f813b88391d8f082b56c091d843cdf
Aidan Ih <aidan.h.ih@gmail.com>
2024-01-22 12:54:56 -0500

    key

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var https = require('https');
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var privateKey  = fs.readFileSync('selfsigned.key', 'utf8');
+var certificate = fs.readFileSync('selfsigned.crt', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
