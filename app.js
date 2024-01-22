cfdb5b4f4f401ce01c041dcb8f08ab5f1849c8eb
Aidan Ih <aidan.h.ih@gmail.com>
2024-01-22 12:51:12 -0500

    https

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-import express from 'express';
-import cors from "cors";
-import StartggWorker from './Startgg/StartggWorker.js';
+var fs = require('fs');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 
-const app = express();
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
 
 app.use(cors());
 app.use(express.json());
+StartggWorker(app);
 app.get('/', (req, res) => {
     res.send('NE Smash Website Start.gg Server')
 })
 
-StartggWorker(app);
-app.listen(4000);
+// your express configuration here
+
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(4000);
+
 console.log("start.gg worker listening on port 4000")
