40f0aa32595daddc65187c612734ebb60d64e90d
Aidan Ih <aidan.h.ih@gmail.com>
2024-01-22 12:03:48 -0500

    logging

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import cors from "cors";
 import StartggWorker from './Startgg/StartggWorker.js';
+
+console.log("starting")
   
 const app = express();
+
+console.log("created app")
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
     res.send('NE Smash Website Start.gg Server')
 })
+
+console.log("starting app")
 StartggWorker(app);
 app.listen(process.env.PORT || 4000);
 
