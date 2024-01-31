3f363edb855723466d7abd116c1d0bba175a3329
Aidan Ih <aidan.h.ih@gmail.com>
2024-01-30 20:37:03 -0500

    send msg before delete

--- Discord/index.js
+++ Discord/index.js
@@ -23,9 +23,10 @@ const runNutbot = () => {
         if (message.author.id === "369305001211854858" || message.author.id === "236270502430113793") {
             const content = message.content;
             if (content === content.toUpperCase()) {
+                message.channel.send("Message From Weegee: " + lower.charAt(0).toUpperCase() + lower.slice(1));
                 message.delete(1000);
                 const lower = content.toLowerCase()
-                message.channel.send("Message From Weegee: " + lower.charAt(0).toUpperCase() + lower.slice(1));
+                
             }
 
         }
