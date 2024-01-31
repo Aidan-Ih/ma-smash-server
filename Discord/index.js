a23f9eabc10eb4702681b3e0bf33c87d8fc6620e
Aidan Ih <aidan.h.ih@gmail.com>
2024-01-31 00:09:18 -0500

    fix

--- Discord/index.js
+++ Discord/index.js
@@ -20,13 +20,12 @@ const runNutbot = () => {
 
     client.on("messageCreate", message => {
         // weegee: 369305001211854858
-        if (message.author.id === "369305001211854858" || message.author.id === "236270502430113793") {
+        if (message.author.id === "369305001211854858") {
             const content = message.content;
             if (content === content.toUpperCase()) {
+                const lower = content.toLowerCase()
                 message.channel.send("Message From Weegee: " + lower.charAt(0).toUpperCase() + lower.slice(1));
                 message.delete(1000);
-                const lower = content.toLowerCase()
-                
             }
 
         }
